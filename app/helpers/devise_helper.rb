2027a4b29ba827fc418cebfc14e93337f5ceceac
Jodie Alaine Parker <me@jodiealaine.com>
2015-01-16 12:53:00 +0000

    Add name field to users

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,7 @@ module DeviseHelper
 	def devise_error_messages!
 		return '' if resource.errors.empty?
 
-		messages = resourse.errors.full_messages.map { |msg| content_tag(:li, msg)}.join
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg)}.join
 		sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resource: resource.class.model_name.human.downcase)
 		html = <<-HTML
 		<div class="alert alert-danger alert-dismissable">
