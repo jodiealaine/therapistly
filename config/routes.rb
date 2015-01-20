8a4b59749f4f1fb3b0408dc1062a82423ccafccf
Jodie Alaine Parker <me@jodiealaine.com>
2015-01-20 12:59:54 +0000

    Create a unique URL for each Listing an Order is placed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :orders
 
   devise_for :users
-  resources :listings
+  resources :listings do 
+    resources :orders
+  end
 
   root 'listings#index'
 
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   get 'pages/contact'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
