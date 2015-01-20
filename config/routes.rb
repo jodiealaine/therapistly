570d7ba796a5639cc0bec46c1eafe406058f2394
Jodie Alaine Parker <me@jodiealaine.com>
2015-01-20 16:14:50 +0000

    Add URLS for /sales and /purchases

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'sales' => "order#sales"
+
+  get 'purchases' => "order#purchases"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
